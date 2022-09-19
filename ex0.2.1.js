d15a0f1be315da3adee08f763d08368a6300e3aa
marcelaproencaeng <104868735+marcelaproencaeng@users.noreply.github.com>
2022-09-19 03:18:57 -0300

    1 file added, 1 deleted

--- ex0.2.1.js
+++ ex0.2.1.js
@@ -3,21 +3,30 @@ const filmesFavoritos = [
     nome: 'Missao Imposivel I',
     genero: 'acao',
     elencoprincipal: 'Tom Cruise',
-    lancamento: 27 - 08 - 2004,
+    lancamento: 2004 - 08 - 27,
     censura: 12,
+    ehTipoString: false,
   },
   {
     nome: 'Jogo de amor em Las Vegas',
     genero: 'comedia',
     elencoPrincipal: 'Ashton KutCher',
-    lancamento: 01 - 05 - 2008,
+    lancamento: 2008 - 05 - 01,
     censura: 12,
+    ehTipoString: false,
   },
   {
     nome: 'Os outros',
     genero: 'suspense',
     elencoPrincipal: 'Nicole Kidman',
     linguagemOriginal: 'ingles',
+    ehTipoString: true,
   },
 ];
-console.log(filmesFavoritos.some((filme) => filme.tipo === 'String'));
+
+const resultado = filmesFavoritos.filter(
+  (filme) => filme.nome.toLowerCase() === 'os outros'
+);
+console.log(resultado);
+
+console.log(filmesFavoritos.some((filme) => filme.ehTipoString === 'true'));
